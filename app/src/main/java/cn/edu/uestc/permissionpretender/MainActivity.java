033c032561890788b30019ee0bf245fd578320da
ZhangGuodong <zhanggd1996@gmail.com>
2018-03-09 22:18:28 +0800

    获取程序列表以及程序相关信息，若干控件有bug

--- app/src/main/java/cn/edu/uestc/permissionpretender/MainActivity.java
+++ app/src/main/java/cn/edu/uestc/permissionpretender/MainActivity.java
@@ -1,5 +1,10 @@
 package cn.edu.uestc.permissionpretender;
 
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,12 +13,38 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * 参考于   http://blog.csdn.net/mp624183768/article/details/76360103
+ */
 
 public class MainActivity extends AppCompatActivity {
+
+    /*
+     * 应用程序集合
+     */
+    private ArrayList<ApplicationInfo> appInfos;
+    private ListView lv_app;
+
+    /*
+     * 管理应用程序包，并通过它获取程序信息
+     */
+    private PackageManager pm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.app_list);
+        pm = getPackageManager();
+
+        /*
+
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -26,6 +57,52 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        */
+    }
+
+    private void initView(){
+        lv_app = (ListView) findViewById(R.id.app_list_view);
+        lv_app.setOnItemClickListener(new AppDetailLinster);
+    }
+
+    private final Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            loadApplications();
+            myHandler.obtainMessage().sendToTarget();
+        }
+    };
+
+    /*
+     * 加载程序列表
+     */
+    private void loadApplications(){
+        PackageManager manager = this.getPackageManager();
+        Intent mainIntent = new Intent(Intent.ACTION_MAIN, null);
+        mainIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+        final List<ResolveInfo> apps = manager.queryIntentActivities(mainIntent, 0);
+        Collections.sort(apps, new ResolveInfo.DisplayNameComparator(manager));
+        if (apps != null){
+            final int count = apps.size();
+            if (appInfos == null){
+                appInfos = new ArrayList<ApplicationInfo>(count);
+            }
+            appInfos.clear();
+            for (int i = 0; i < count; i++){
+                ApplicationInfo application = new ApplicationInfo();
+                ResolveInfo info = apps.get(i);
+                application.
+                application.title = info.loadLabel(manager);
+                application.setActivity(new ComponentName(
+                        info.activityInfo.applicationInfo.packageName,
+                        info.activityInfo.name), Intent.FLAG_ACTIVITY_NEW_TASK
+                        | Intent.FLAG_ACTIVITY_RESET_TASK_IF_NEEDED);
+                application.icon = info.activityInfo.loadIcon(manager);
+                application.packageName = info.activityInfo.applicationInfo.packageName;
+                appInfos.add(application);
+            }
+        }
     }
 
     @Override
