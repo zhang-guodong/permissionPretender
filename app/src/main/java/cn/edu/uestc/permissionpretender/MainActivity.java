905f76b256de9c86ac2033d0fdfe53e0d556fb3f
zhang-guodong <zhangguodong1996@qq.com>
2018-03-10 23:14:45 +0800

    参考手机安全助手项目学习应用管理

--- app/src/main/java/cn/edu/uestc/permissionpretender/MainActivity.java
+++ app/src/main/java/cn/edu/uestc/permissionpretender/MainActivity.java
@@ -1,13 +1,20 @@
 package cn.edu.uestc.permissionpretender;
 
+import android.content.Context;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.SparseIntArray;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -48,5 +55,44 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public static ArrayList<ApplicationInfo> getAppInfo(Context ctx, int type) {
+        ArrayList<ApplicationInfo> appList = new ArrayList<ApplicationInfo>();
+        SparseIntArray siArray = new SparseIntArray();
+        PackageManager pm = ctx.getPackageManager();
+        List<ApplicationInfo> installList = pm.getInstalledApplications(PackageManager.PERMISSION_GRANTED);
+        for (int i = 0; i < installList.size(); i++){
+            ApplicationInfo item = installList.get(i);
+            if (siArray.indexOfKey(item.uid) >= 0){ //去掉重复的应用信息
+                continue;
+            }
+            siArray.put(item.uid, 1);
+            try{
+                String[] permissions = pm.getPackageInfo(item.processName, PackageManager.GET_PERMISSIONS).requestedPermissions;
+                if (permissions == null){
+                    continue;
+                }
+                boolean bNet = false;
+                for (String permission : permissions){
+                    if (permission.equals("android.permission.INTERNET")){
+                        bNet = true;
+                        break;
+                    }
+                }
+                if (type == 0 || (type == 1 && bNet)){
+                    ApplicationInfo app = new ApplicationInfo();
+                    app.uid = item.uid;
+                    //TODO
+                    app.label = item.loadLabel(pm).toString();
+                    app.package_name = item.packageName;
+                    appList.add(app);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+                continue;
+            }
+        }
+        return appList;
     }
 }
